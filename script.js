4fc3e43875235ad03cda641a9908cfed3a97f757
Rajudas575 <rajudas575@gmail.com>
2024-11-25 10:40:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
 //your JS code here. If required.
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const inputs = document.querySelectorAll('.code');
     
     inputs.forEach((input, index) => {
-    input.addEventListener('input', (e) => {
-        if (e.target.value) {
-            // Move focus to next input if a value is entered
-            if (index < inputs.length - 1) {
-                setTimeout(() => {
+        input.addEventListener('input', async (e) => {
+            if (e.target.value) {
+                // Move focus to next input if a value is entered
+                if (index < inputs.length - 1) {
+                    await delay(100);
                     inputs[index + 1].focus();
-                    const event = new Event('input', { bubbles: true });
-                    inputs[index + 1].dispatchEvent(event);
-                }, 100); // delay of 100 milliseconds
+                }
             }
-        }
-    });
+        });
 
-    input.addEventListener('keydown', (e) => {
-        if (e.key === 'Backspace') {
-            // Move focus to previous input if backspace is pressed
-            if (index > 0) {
-                setTimeout(() => {
+        input.addEventListener('keydown', async (e) => {
+            if (e.key === 'Backspace') {
+                // Move focus to previous input if backspace is pressed
+                if (index > 0) {
+                    await delay(100);
                     inputs[index - 1].focus();
-                    const event = new Event('input', { bubbles: true });
-                    inputs[index - 1].dispatchEvent(event);
-                }, 100); // delay of 100 milliseconds
+                }
             }
-        }
+        });
     });
-});
 });
