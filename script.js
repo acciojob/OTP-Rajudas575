3819d48144353ecac159c3737c5ebf87c94d0ef6
Rajudas575 <rajudas575@gmail.com>
2024-11-25 10:27:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 //your JS code here. If required.
-document.addEventListener("DOMContentLoaded", () => {
-    const inputs = document.querySelectorAll('.code');
-    
-    inputs.forEach((input, index) => {
-        input.addEventListener('input', (e) => {
-            if (e.target.value) {
-                // Move focus to next input if a value is entered
-                if (index < inputs.length - 1) {
-                    inputs[index + 1].focus();
-                }
+inputs.forEach((input, index) => {
+    input.addEventListener('input', (e) => {
+        if (e.target.value) {
+            // Move focus to next input if a value is entered
+            if (index < inputs.length - 1) {
+                inputs[index + 1].focus();
+                const event = new Event('input', { bubbles: true });
+                inputs[index + 1].dispatchEvent(event);
             }
-        });
-
-        input.addEventListener('keydown', (e) => {
-    if (e.key === 'Backspace') {
-        // Move focus to previous input if backspace is pressed
-        if (index > 0) {
-            inputs[index - 1].focus();
         }
-    }
-});
+    });
+
+    input.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace') {
+            // Move focus to previous input if backspace is pressed
+            if (index > 0) {
+                inputs[index - 1].focus();
+                const event = new Event('input', { bubbles: true });
+                inputs[index - 1].dispatchEvent(event);
+            }
+        }
     });
 });
